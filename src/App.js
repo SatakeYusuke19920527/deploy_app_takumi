7f929242e5bffb01440dbf350df72ab94999b1d2
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-08-07 20:56:54 +0900

    button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+const [count, setCount] = useState(0)
 
 const handleClick = () => {
   console.log("Hello world")
   alert("Hello would")
 }
 
-function App() {
+const Increment = () => {
+  setCount(count + 1)
+}
+  
+const Decrement = () => {
+  setCount(count - 1)
+}
+  
+const Reset = () => {
+  setCount(count === 0)
+}  
   return (
     <div>
-      <button onClick={handleClick}>hello world</button>
+      <p><button onClick={handleClick}>hello world</button></p>
+      <p><button onClick={Increment}>いいね！</button><button onClick={Decrement}>よくないね！</button><button onClick={Reset}>reset</button></p>
+      <p>{count}</p>
     </div>
   );
 }
 
 export default App;
+
