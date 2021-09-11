b6b181da60bfd63aa6dad676829d306ecfff8df9
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-09-11 20:13:55 +0900

    ジャンケンできた

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [countcpu, setCountcpu] = useState(0)
   const [text, setText] = useState('')
   const [text1, setText1] = useState('')
   const [error, setError] = useState('')
+  const [jibun, setJibun] = useState('')
+  const [cpu, setCpu] = useState('')
+  const [kekka, setKekka] = useState('')
   
 
   const handleClick = () => {
@@ -37,11 +41,60 @@ function App() {
     console.log(error)
     if (text === "") {
       setError('何か文字を入力してください')
+      setText1('')
     } else {
       setText1(text)
       setError('')
     }
   }
+
+  const jankenjibun = () => {
+    setJibun('ぐー')
+    jankencpu()
+    if (countcpu === 0) {
+      setKekka('あいこです。')
+    } else if (countcpu === 1) {
+      setKekka('かちです。')
+    } else if (countcpu === 2) {
+      setKekka('まけです。')
+    }
+  }
+  const jankenjibun1 = () => {
+    setJibun('ちょき')
+    jankencpu()
+    if (countcpu === 0) {
+      setKekka('まけです。')
+    } else if (countcpu === 1) {
+      setKekka('あいこです。')
+    } else if (countcpu === 2) {
+      setKekka('かちです。')
+    }
+  }
+  const jankenjibun2 = () => {
+    setJibun('ぱー')
+    jankencpu()
+    if (countcpu === 0) {
+      setKekka('かちです。')
+    } else if (countcpu === 1) {
+      setKekka('まけです。')
+    } else if (countcpu === 2) {
+      setKekka('あいこです。')
+    }
+  }
+  const jankencpu = () => {
+    const cpuCard = Math.floor(Math.random() * 3)
+    setCountcpu(cpuCard)
+    console.log(countcpu)
+    if (countcpu === 0) {
+      setCpu('ぐー')
+    } else if (countcpu === 1){
+      setCpu('ちょき')
+    } else if (countcpu === 2){
+      setCpu('ぱー')
+    }
+  }
+
+
   
   
     return(
@@ -49,10 +102,12 @@ function App() {
       <p><button onClick={handleClick}>hello world</button></p>
       <p><button onClick={Increment}>いいね！</button><button onClick={Decrement}>よくないね！</button><button onClick={Reset}>reset</button></p>
       <p>{count}</p>
-        <input type="text" value={text} onChange={handleChange} />
-        <button onClick={hyouji}>Hello</button>
-        <h1>{text1}{error}</h1>
-        
+      <p><input type="text" value={text} onChange={handleChange} /><button onClick={hyouji}>Hello</button><p>{text1}</p></p>
+        <p style={{ color: 'red' }}>{error}</p>
+        <p><button onClick={jankenjibun}>ぐー</button><button onClick={jankenjibun1}>ちょき</button><button onClick={jankenjibun2}>ぱー</button></p>
+        <p>あなたの手：{jibun}</p>
+        <p>cpuの手：{cpu}</p>
+        <p>勝敗：{kekka}</p>
     </div >
   );
 }
